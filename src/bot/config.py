cad5f822d852d8344f0cb182aed676ad7dbc906d
BiasedKiwi <alexandre1920911@gmail.com>
2023-08-06 09:55:02 +0200

    Fixed config.yaml not found error

--- src/bot/config.py
+++ src/bot/config.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 import yaml
 
 
-def get_raw_config(path: str = "./bot/config.yaml"):
+def get_raw_config(path: str = Path(__file__).parent / "config.yaml"):
     """Return a raw dict of the config file (using `yaml.load`)"""
     with open(path, encoding="utf-8") as file:
         _dict = yaml.load(file, Loader=yaml.FullLoader)
